9dd9e0f53583be4043dee21b529fd329bdd338ba
Carolina Borges Cardoso <borges_carolina@sicredi.com.br>
2022-04-29 10:29:23 -0300

    ajuste em maiorLanceDadoNoLeilao, finaliza implementacao do teste

--- src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
+++ src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
@@ -1,5 +1,6 @@
 package br.com.alura.leilao.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +30,9 @@ public class FinalizarLeilaoService {
 	}
 
 	private Lance maiorLanceDadoNoLeilao(Leilao leilao) {
-		List<Lance> lancesDoLeilao = leilao.getLances();
+
+									//modifica para new ArrayList para aceitar o método sort (antes era uma lista imutável que n aceita sort)
+		List<Lance> lancesDoLeilao = new ArrayList<>(leilao.getLances());
 		lancesDoLeilao.sort((lance1, lance2) -> {
 			return lance2.getValor().compareTo(lance1.getValor());
 		});
